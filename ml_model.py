a426615eb8e2a2a635a577bc68b832b8ac9d20e9
Shakti0502 <shakti0502@gmail.com>
2022-08-12 11:34:51 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,13 @@
 
 
 print("addit all ml realtred coide here")
+
+def model1():
+    print("adding code for ML Model1")
+
+
+def model2():
+    print("adding code for ML model2")
+
+def model3():
+    print("adding code for ML model3")
